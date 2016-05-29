069b59e97dc8bd40815d61e59ba3c7ec57d1717e
Nataly Shuster <shuster.nata@gmail.com>
2016-05-29 18:58:20 +0300

    реализовать класс контейнер игрового поля - реализован метод printField

--- crenul/src/crenul/PlayField.java
+++ crenul/src/crenul/PlayField.java
@@ -8,10 +8,32 @@ public class PlayField {
 		System.out.println("Создаем новое поле...");
 		for(int i = 0; i < 3; i++){
 			ArrayList<String> str = new ArrayList<String>(3);
+			for(int j = 0; j < 3; j++){
+			str.add(" ");
+			}	
 			field.add(str);
 			System.out.println("Добавлена строка: " + i);
 		}
 		System.out.println("Готово");
 	}
-
+    public void printField() {
+    	System.out.println("  | 0 | 1 | 2 |");
+    	System.out.println("________________");
+    	System.out.print("a");
+    	for(int i = 0; i < 3; i++){
+    		System.out.print(" | " + field.get(0).get(i));	
+    	}
+    	System.out.println(" |");
+    	System.out.print("b");
+    	for(int i = 0; i < 3; i++){
+    		System.out.print(" | " + field.get(1).get(i));	
+    	}
+    	System.out.println(" |");
+    	System.out.print("c");
+    	for(int i = 0; i < 3; i++){
+    		System.out.print(" | " + field.get(2).get(i));	
+    	}
+    	System.out.println(" |");
+    	
+    }
 }
